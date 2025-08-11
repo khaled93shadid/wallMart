e8a5d3a3ac3d2155411489f2fd754595ce30828c
khaled93shadid <khaled_shadid@yahoo.com>
2025-08-11 16:10:45 +0300

    body div1 finish

--- client/src/components/main.js
+++ client/src/components/main.js
@@ -7,32 +7,90 @@ return(
 <>
 <Navbar />
 <div className='main_body_div'>{/*body start */}
-    <div className='body_div1_container'>{/*CONTAINER START */}
-       <div className='body_div1'>{/*DIV1 START */}
-             <div className='body_div1_1'>{/*start */}
-                   <img className='body_div1_1_img' src='https://i5.walmartimages.com/dfw/4ff9c6c9-462c/k2-_d78507e5-bb63-49d3-83b0-08c12886986e.v1.jpg?odnHeight=256&odnWidth=385&odnBg=&odnDynImageQuality=70'/>
-                   <p className='body_div1_1_p'>College tech </p>  
-                   <p className='body_div1_1_p2'>starting at $19.88</p>  
-                 <a href=''> <p className='body_div1_1_p3'>shop tech</p> </a> 
-             </div>{/*END */}
-             <div className='body_div1_2'>{/*start */}
-                       <img className='body_div1_2_img' src='https://i5.walmartimages.com/dfw/4ff9c6c9-3835/k2-_1c192789-e622-465b-8e73-8000c7d68982.v1.jpg?odnHeight=578&odnWidth=385&odnBg=&odnDynImageQuality=70' />
-                       <p className='body_div1_2_p3'>Backpacks from Wonder</p>
-                       <p className='body_div1_2_p4'>Nation,pokemon & more</p>
-                       <a href=''> <p className='body_div1_3_p3'>shop now</p> </a> 
-                       <p className='body_div1_2_p1'>From</p>
-                       <p className='body_div1_2_p2'>$<span className='body_div1_2_span'>5</span></p>
-             </div>{/*END */}
-             <div className='body_div1_3'>{/*start */}
-                   
-                    <img className='body_div1_3_img' src='https://i5.walmartimages.com/dfw/4ff9c6c9-d211/k2-_e5b67f41-878e-498a-801f-b012d8b3b838.v1.jpg?odnHeight=216&odnWidth=385&odnBg=&odnDynImageQuality=70'/>
-                   <p className='body_div1_3_p'>Hot,new beauty </p>  
-                   <p className='body_div1_3_p2'>from $10</p>  
-                 <a href=''> <p className='body_div1_3_p3'>shop now</p> </a> 
-             </div>{/*END */}
-       </div>{/*DIV1 END */}
-             
-    </div>{/*CONTAINER END */}
+      <div className='body_div1_container'>{/*CONTAINER div1 START */}
+        <div className='body_div1'>{/*DIV1 START */}
+              <div className='body_div1_1'>{/*start */}
+                    <img className='body_div1_1_img' src='https://i5.walmartimages.com/dfw/4ff9c6c9-462c/k2-_d78507e5-bb63-49d3-83b0-08c12886986e.v1.jpg?odnHeight=256&odnWidth=385&odnBg=&odnDynImageQuality=70'/>
+                    <p className='body_div1_1_p'>College tech </p>  
+                    <p className='body_div1_1_p2'>starting at $19.88</p>  
+                  <a href=''> <p className='body_div1_1_p3'>shop tech</p> </a> 
+              </div>{/*END */}
+              <div className='body_div1_2'>{/*start */}
+                        <img className='body_div1_2_img' src='https://i5.walmartimages.com/dfw/4ff9c6c9-3835/k2-_1c192789-e622-465b-8e73-8000c7d68982.v1.jpg?odnHeight=578&odnWidth=385&odnBg=&odnDynImageQuality=70' />
+                        <p className='body_div1_2_p3'>Backpacks from Wonder</p>
+                        <p className='body_div1_2_p4'>Nation,pokemon & more</p>
+                        <a href=''> <p className='body_div1_3_p3'>shop now</p> </a> 
+                        <p className='body_div1_2_p1'>From</p>
+                        <p className='body_div1_2_p2'>$<span className='body_div1_2_span'>5</span></p>
+              </div>{/*END */}
+              <div className='body_div1_3'>{/*start */}
+                    
+                      <img className='body_div1_3_img' src='https://i5.walmartimages.com/dfw/4ff9c6c9-d211/k2-_e5b67f41-878e-498a-801f-b012d8b3b838.v1.jpg?odnHeight=216&odnWidth=385&odnBg=&odnDynImageQuality=70'/>
+                    <p className='body_div1_3_p'>Hot,new beauty </p>  
+                    <p className='body_div1_3_p2'>from $10</p>  
+                  <a href=''> <p className='body_div1_3_p3'>shop now</p> </a> 
+              </div>{/*END */}
+        </div>{/*DIV1 END */}
+
+
+        <div className='body_div2'>{/*DIV2 START */}
+              <div className='body_div2_1'>{/*start */}
+                  <img className='body_div2_1_img' src='https://i5.walmartimages.com/dfw/4ff9c6c9-4b8c/k2-_92c5d71b-2cf1-477f-bf76-b6b6254d465c.v1.jpg?odnHeight=447&odnWidth=794&odnBg=&odnDynImageQuality=70' />
+                  <p className='body_div2_1_p1'>Get it in as fast as an hour*</p>
+                  <p className='body_div2_1_p2'>Dinner made</p>
+                  <p className='body_div2_1_p3'>easy with delivery</p>
+                  <p className='body_div2_1_p4'>shop food</p>
+              </div>{/*end */}
+
+              <div className='body_div2_2'>{/*start */}
+                  <div className='div_relative'>
+                    <img className='body_div2_2_img' src='https://i5.walmartimages.com/dfw/4ff9c6c9-ca2a/k2-_9c130100-4840-46b4-bd80-3781b36ef9fa.v1.jpg?odnHeight=256&odnWidth=385&odnBg=&odnDynImageQuality=70' />
+                    <p className='body_div2_2_p'>Top 100 school picks</p>
+                    <a href=''>  <p className='body_div2_2_p2'>shop now</p> </a> 
+                  </div>
+                  <div className='div_relative'>
+                    <img className='body_div2_2_img' src='https://i5.walmartimages.com/dfw/4ff9c6c9-57e3/k2-_8308f9bf-4f45-4862-bd1e-dd83507e4d27.v1.png?odnHeight=388&odnWidth=385&odnBg=&odnDynImageQuality=70' />
+                    <p className='body_div2_2_p'>Up to 50% off</p>
+                    <a href=''>  <p className='body_div2_2_p2'>shop now</p> </a> 
+                  </div>
+                  <div className='div_relative3'>
+                        <div className='div_relative3_1'> 
+                            <p className='div_relative3_p'>Free delivery from Walmart+ makes life easier</p>
+                          <a className='div_relative3_a' href=''> <p className='div_relative3_p2'>Try Walmart+ for free </p> </a>
+                        </div>
+                        <img className='div_relative3_img' src='https://i5.walmartimages.com/dfw/4ff9c6c9-b5af/k2-_83cc8462-cf08-472f-98a2-745791f77752.v1.jpg?odnBound=720' />
+                  </div>
+  
+              </div>{/*end*/}
+                
+        </div>{/*DIV2 END */}
+
+        <div className='body_div3'>{/*DIV3 start */}
+              <div className='body_div3_1'>
+                <img className='body_div3_img' src='https://i5.walmartimages.com/dfw/4ff9c6c9-f19f/k2-_aa251f25-d877-4cf6-bf29-427a2199be65.v1.jpg?odnHeight=216&odnWidth=385&odnBg=&odnDynImageQuality=70' />
+                <p className='body_div3_p'>From $5:</p>
+                <p className='body_div3_p2'>backpacks from</p>
+                <p className='body_div3_p3'>top brands</p>
+                <p className='body_div3_p4'>shop now</p>
+
+              </div>
+              <div className='body_div3_2'>
+                <img className='body_div3_img' src='https://i5.walmartimages.com/dfw/4ff9c6c9-1157/k2-_ef2e5ae1-f4b4-4a20-9dd5-6c280debc4be.v1.jpg?odnHeight=256&odnWidth=385&odnBg=&odnDynImageQuality=70' />
+                <p className='body_div3_p'>Tween trends_</p>
+                <p className='body_div3_p2'>from $5.86</p>
+                <p className='body_div3_p44'>shop now</p>
+
+              </div>
+              <div className='body_div3_3'>
+                <img className='body_div3_3_img' src='https://i5.walmartimages.com/dfw/4ff9c6c9-88f4/k2-_79e6f99a-9ea6-4c83-8227-e63c495408d0.v1.jpg?odnHeight=578&odnWidth=385&odnBg=&odnDynImageQuality=70' />
+                <p className='body_div3_p'>Stylish dorm</p>
+                <p className='body_div3_p2'>furniture</p> 
+                <p className='body_div3_3_p44'>shop now</p>
+
+              </div>
+        </div>{/*DIV3 END */}
+              
+      </div>{/*CONTAINER div1 END */}
 
 </div>{/*body End */}
 
